1882e81116b39f00a09c03218a1852120cb4422b
Luis Gonzalez <luis.gonzalez@minerva.kgi.edu>
2018-06-22 17:30:43 +0900

    Commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
-
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyBIlQQJsnVP-wvBymyubY3eZpg85q6jaMw';
 
 
@@ -12,17 +12,26 @@ class App extends Component {
   constructor(props){
     super(props);
 
-    this.state = { videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
-    YTSearch({key: API_KEY, term: 'potatoes'}, (videos) => {
-      this.setState({ videos });
+    YTSearch({key: API_KEY, term: 'fortnite'}, (videos) => {
+      this.setState({
+      videos: videos,
+      selectedVideo: videos[0]
+      });
     });
   }
   render() {
   return (
     <div>
       <SearchBar />
-      <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+      <VideoList
+        onVideoSelect ={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
     </div>
     );
   }
